14a319ecc8d5690e054cff4e6a6a8c08404a7c37
Hannes Aasamets <hannes.aasamets@bigbank.ee>
2024-04-22 15:56:18 +0300

    Install PrimeVue unstyled.

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,14 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
 import App from './App.vue'
 
 const app = createApp(App)
 
+app.use(PrimeVue, {
+    unstyled: true,
+})
 app.use(createPinia())
 
 app.mount('#app')
