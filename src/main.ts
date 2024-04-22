e2bf26b55f142250590abcc9e9ad6c5d957abb6c
Hannes Aasamets <hannes.aasamets@bigbank.ee>
2024-04-22 15:58:52 +0300

    Download full PrimeVue presets lara & wind v0.8.2.

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
+import Lara from '@/presets/lara'
 import App from './App.vue'
 
 const app = createApp(App)
 
 app.use(PrimeVue, {
     unstyled: true,
+    pt: Lara,
 })
 app.use(createPinia())
 
